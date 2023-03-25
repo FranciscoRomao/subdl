11cb6ae0954c403b03a71613b461e55c46f6082e
Milan Hauth <milahu@gmail.com>
2023-03-25 18:13:19 +0100

    format VERSION_INFO

--- subdl.py
+++ subdl.py
@@ -9,8 +9,8 @@ VERSION = "1.1.2"
 
 VERSION_INFO = """\
 
-This is free software; see the source for copying conditions.  There is NO
-warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+This is free software; see the source for copying conditions.
+There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
 https://github.com/alexanderwink/subdl
 """
